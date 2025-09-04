35c12aa3405b54223fe67dff6deb9edf650d21ef
Priti Aware <pritiaware.datagrid@gmail.com>
2025-09-04 18:19:46 +0530

    dashboard-image added

--- resources/views/Auth/Admin-login/admin.blade.php
+++ resources/views/Auth/Admin-login/admin.blade.php
@@ -67,6 +67,9 @@ height:100%;
 }
 
 h4{
+    display: flex;
+    flex-direction:column;
+    align-items:center;
     padding:2rem 2rem;
     box-shadow: 0 2px 4px 0 rgba(0,0,0,.1);
 }
@@ -106,8 +109,13 @@ h4{
             <div class="home">
          <h3>Overview</h3>
          <div class="dashboard-text">
- <h4>Total Family count:{{$totalFamilies}} </h4>
-        <h4>Total member count: {{$totalMembers}} </h4>
+        <h4>
+        <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#1f1f1f"><path d="M185-80q-17 0-29.5-12.5T143-122v-105q0-90 56-159t144-88q-40 28-62 70.5T259-312v190q0 11 3 22t10 20h-87Zm147 0q-17 0-29.5-12.5T290-122v-190q0-70 49.5-119T459-480h189q70 0 119 49t49 119v64q0 70-49 119T648-80H332Zm148-484q-66 0-112-46t-46-112q0-66 46-112t112-46q66 0 112 46t46 112q0 66-46 112t-112 46Z"/></svg><br>    
+        Total Family count:{{$totalFamilies}} 
+         </h4>
+        <h4>
+            <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#1f1f1f"><path d="M240-320q-33 0-56.5-23.5T160-400q0-33 23.5-56.5T240-480q33 0 56.5 23.5T320-400q0 33-23.5 56.5T240-320Zm480 0q-33 0-56.5-23.5T640-400q0-33 23.5-56.5T720-480q33 0 56.5 23.5T800-400q0 33-23.5 56.5T720-320Zm-240-40q-42 0-71-29t-29-71q0-42 29-71t71-29q42 0 71 29t29 71q0 42-29 71t-71 29ZM284-120q14-69 68.5-114.5T480-280q73 0 127.5 45.5T676-120H284Zm-204 0q0-66 47-113t113-47q17 0 32 3t29 9q-30 29-50 66.5T224-120H80Zm656 0q-7-44-27-81.5T659-268q14-6 29-9t32-3q66 0 113 47t47 113H736ZM88-480l-48-64 440-336 160 122v-82h120v174l160 122-48 64-392-299L88-480Z"/></svg><br> 
+        Total member count: {{$totalMembers}} </h4>
          </div>
        
             </div>
